78fa75740e03f199d6f6452539030647a9161383
sunilsp77 <sunil.thind17@gmail.com>
2020-03-13 00:43:22 +0530

    some changes until today

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Router } from "react-router-dom";
+// import Clock from "./Clock";
+import history from "./services/history";
+import Routes from "./routes";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>I am React App</h1>
+    <div className="notificationsFrame">
+      <div className="panel">
+        <Header className="demo" />
+      </div>
     </div>
   );
 }
